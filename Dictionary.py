e0f2789ba1cdaf425385563d2ebebef81e107fa5
Itsuhi <53667742+itsuhi@users.noreply.github.com>
2021-05-26 13:38:02 +0900

    dictionary project complete

--- Dictionary.py
+++ Dictionary.py
@@ -66,3 +66,45 @@ print(isValidChessboard(test))
 
 #=======================================================================================================================
 
+"""
+Fantasy Game Inventory
+
+You are creating a fantasy video game. The data structure to model the player’s inventory will be a dictionary where the
+keys are string values describing the item in the inventory and the value is an integer value detailing how many of that 
+item the player has. For example, the dictionary value {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} 
+means the player has 1 rope, 6 torches, 42 gold coins, and so on.
+
+Write a function named displayInventory() that would take any possible “inventory” and display it like the following:
+"""
+
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(v, k)
+        item_total = item_total + v
+    print("Total number of items: " + str(item_total))
+
+
+#displayInventory(stuff)
+
+def addToInventory(inventory, addedItems):
+    """
+    :param inventory: dictionary representing the player’s inventory
+    :param addedItems: list of loot
+    :return: updated inventory
+    """
+    for item in addedItems:
+        if item in inventory:
+            inventory[item] = inventory[item] + 1
+        else:
+            inventory[item] = 1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
